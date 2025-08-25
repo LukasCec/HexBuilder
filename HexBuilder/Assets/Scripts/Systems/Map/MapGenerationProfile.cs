088f3669923f629b3d2fc90493a3c84a7d8ad47c
Lukas Cec <lukinecka123987@gmail.com>
2025-08-25 16:09:46 +0200

    scene & assets ready for hex map generator

---
+++ HexBuilder/Assets/Scripts/Systems/Map/MapGenerationProfile.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace HexBuilder.Systems.Map
+{
+   
+    [CreateAssetMenu(fileName = "MapGenerationProfile", menuName = "HexBuilder/Map Generation Profile", order = 1)]
+    public class MapGenerationProfile : ScriptableObject
+    {
+        
+    }
+}
---
+++ HexBuilder/Assets/Scripts/Systems/Map/TerrainType.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace HexBuilder.Systems.Map
+{
+    
+    [CreateAssetMenu(fileName = "TerrainType", menuName = "HexBuilder/Terrain Type", order = 0)]
+    public class TerrainType : ScriptableObject
+    {
+        
+    }
+}
