bd2ca545ca75e2c0df386a9bbd011eaba07a4d49
Lukas Cec <lukinecka123987@gmail.com>
2025-08-27 16:22:20 +0200

    Fixes #9: [UI] Add Build/Select mode switch

--- HexBuilder/Assets/Scripts/UI/BuildModeUI.cs
+++ HexBuilder/Assets/Scripts/UI/BuildModeUI.cs
@@ -6,15 +6,27 @@ namespace HexBuilder.UI
     public class BuildModeUI : MonoBehaviour
     {
         public BuildingPlacer placer;
+        public BuildPanelAnimator buildPanel;   
 
         public void OnSelectModeClick()
         {
             if (placer) placer.ExitBuildMode();
+            if (buildPanel) buildPanel.Hide();  
         }
 
-        public void OnBuildModeClick(BuildingType type)
+       
+        public void OnBuildModeClick()
+        {
+            if (buildPanel) buildPanel.Show();  
+            
+            if (placer) placer.EnterBuildMode(null); 
+        }
+
+        
+        public void OnPickBuilding(BuildingType type)
         {
             if (placer && type) placer.EnterBuildMode(type);
+            if (buildPanel) buildPanel.Show();
         }
     }
 }
